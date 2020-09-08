11168beacf3a6e359057d8a7d691bee1ffe05d6c
Florian Richter <florian.richter.de@gmail.com>
2020-09-08 17:01:48 +0200

    added color to return

--- backend/src/scoreboard/scoreboard.controller.ts
+++ backend/src/scoreboard/scoreboard.controller.ts
@@ -26,10 +26,14 @@ export class ScoreboardController {
               status: event.competitions[0].status.type.name,
               home: {
                 name: home.team.displayName,
+                color: home.team.color,
+                color2: home.team.alternateColor,
                 score: home.score,
               },
               away: {
                 name: away.team.displayName,
+                color: away.team.color,
+                color2: away.team.alternateColor,
                 score: away.score,
               },
             };
