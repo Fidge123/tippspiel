fbac37cdd11322523398f55b97da739d37a6a976
Florian Richter <florian.richter.de@gmail.com>
2020-09-08 11:45:56 +0200

    Add login button

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain="withered-salad-9484.eu.auth0.com"
+    clientId="VvT2n6cFCwL1pCTAxWdmZJuVPDwOGl3r"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
