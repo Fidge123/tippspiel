f0be9840820f936c7d7cfeeac054e325ecad835b
Florian Richter <florian.richter.de@gmail.com>
2020-09-08 10:00:55 +0200

    Updates

---
+++ precache-manifest.e7bd5b996bf44e901809ae02ef8886dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c6c3fcfc858b4f320bab588073b2ec",
+    "url": "/tippspiel/index.html"
+  },
+  {
+    "revision": "4c71232079428e61f6e7",
+    "url": "/tippspiel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/tippspiel/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tippspiel/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c71232079428e61f6e7",
+    "url": "/tippspiel/static/js/main.f3af9571.chunk.js"
+  },
+  {
+    "revision": "14ca355c90f43e6d9185",
+    "url": "/tippspiel/static/js/runtime-main.f64af798.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tippspiel/static/media/logo.5d5d9eef.svg"
+  }
+]);
