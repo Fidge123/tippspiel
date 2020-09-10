0d9592532606dc440255f46555dcb49870614aec
Florian Richter <florian.richter.de@gmail.com>
2020-09-10 11:22:58 +0200

    Debug

--- backend/src/user.decorator.ts
+++ backend/src/user.decorator.ts
@@ -3,11 +3,13 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    return request;
   },
 );
 
 export interface User {
+  username: string;
   name: string;
+  nickname: string;
   email: string;
 }
