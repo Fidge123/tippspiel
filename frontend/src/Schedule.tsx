f93ccb881d92fb177730f9d8a4e9dc865ba2beaf
Florian Richter <florian.richter.de@gmail.com>
2020-09-11 10:16:45 +0200

    Add audience again

--- frontend/src/Schedule.tsx
+++ frontend/src/Schedule.tsx
@@ -180,6 +180,7 @@ function Schedule() {
       }
 
       const token = await getAccessTokenSilently({
+        audience: "https://nfl-tippspiel.herokuapp.com/auth",
         scope: "read:tipp",
       });
       const response = await fetch(BASE_URL + "tipp", {
@@ -193,6 +194,7 @@ function Schedule() {
 
   async function postTipp(payload: string) {
     const token = await getAccessTokenSilently({
+      audience: "https://nfl-tippspiel.herokuapp.com/auth",
       scope: "write:tipp",
     });
 
@@ -210,10 +212,12 @@ function Schedule() {
     let token;
     try {
       token = await getAccessTokenSilently({
+        audience: "https://nfl-tippspiel.herokuapp.com/auth",
         scope: "write:schedule",
       });
     } catch (e) {
       token = await getAccessTokenWithPopup({
+        audience: "https://nfl-tippspiel.herokuapp.com/auth",
         scope: "write:schedule",
       });
     }
