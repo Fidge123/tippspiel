23456490d41c5a06bad507fff3d9fcdfa880528d
Florian Richter <florian.richter.de@gmail.com>
2020-09-12 16:38:00 +0200

    Fix leaderboard

--- backend/src/tipp/leaderboard.controller.ts
+++ backend/src/tipp/leaderboard.controller.ts
@@ -16,9 +16,9 @@ export class LeaderboardController {
     private readonly sbService: ScoreboardService,
   ) {}
 
-  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
+  // @UseGuards(AuthGuard('jwt'), PermissionsGuard)
   @Get(':season')
-  @Permissions('read:tipp')
+  // @Permissions('read:tipp')
   async getAll(@Param('season') season: string): Promise<any> {
     const games = await this.sbService.findFinished(parseInt(season, 10));
     const users = await this.userService.findAll();
@@ -34,32 +34,31 @@ export class LeaderboardController {
         parseInt(game.competitors[1].score, 10);
       const points = tipps
         .sort((a, b) => {
-          if (
-            a.winner === winner &&
-            Math.abs(a.pointDiff - pointDiff) -
+          if (a.winner === winner) {
+            return (
+              Math.abs(a.pointDiff - pointDiff) -
               Math.abs(b.pointDiff - pointDiff)
-          ) {
-            return -1;
+            );
           } else {
             return 1;
           }
         })
         .map((t, i) => ({
+          winner: t.winner,
           user: users.find(u => u.email === t.user)?.name || t.user,
           pointDiff: Math.abs(t.pointDiff - pointDiff),
           points: t.winner === winner ? 1 + Math.max(3 - i, 0) : 0,
         }));
 
-      points.forEach((t, i) => {
-        if (i > 0 && t.pointDiff === points[i].pointDiff) {
-          t.points = points[i].points;
-        }
+      points.forEach((c, i) => {
+        const p =
+          i > 0 && c.points > 0 && c.pointDiff === points[i - 1].pointDiff
+            ? points[i - 1].points
+            : c.points;
+        lb[c.user] = { ...lb[c.user], [game.id]: p };
       });
 
-      return points.reduce((r, c) => {
-        lb[c.user] = { ...lb[c.user], [game.id]: c.points };
-        return lb;
-      }, {});
+      return lb;
     }, {} as any);
   }
 }
