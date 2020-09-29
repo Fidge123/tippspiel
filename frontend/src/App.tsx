8809480e12f229b1b792357b367d439ed7732ac2
Florian Richter <florian.richter.de@gmail.com>
2020-09-29 17:44:21 +0200

    Fix header color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <span>Tippspiel</span>
+        Tippspiel
         {isLoading ? (
           <span className="loading">loading...</span>
         ) : isAuthenticated ? (
