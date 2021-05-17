2b979ca1551610e154dcc005c7bc13b37dc7c572
Florian Richter <florian.richter.de@gmail.com>
2021-05-17 20:55:07 +0200

    Another SSL fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -18,8 +18,9 @@ import { UserModule } from './user/user.module';
       url: env.DATABASE_URL,
       entities: [ScoreboardEntity, Tipp, User],
       synchronize: true,
+      ssl: !env.DATABASE_URL.includes('localhost'),
       extra: {
-        ssl: !env.DATABASE_URL.includes('localhost'),
+        ssl: { rejectUnauthorized: false },
       },
     }),
     ScoreboardModule,
