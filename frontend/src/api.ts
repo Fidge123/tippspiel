b628f5bacc1c72448f4b97bcdd6d3c7732d983aa
Florian Richter <florian.richter.de@gmail.com>
2021-07-09 09:49:44 +0200

    Use process.env instead of import

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,2 @@
-import { env } from "process";
-
 export const BASE_URL =
-  env.REACT_APP_API_URL || "https://nfl-tippspiel.herokuapp.com/";
+  process.env.REACT_APP_API_URL || "https://nfl-tippspiel.herokuapp.com/";
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { env } from "process";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain={env.REACT_APP_AUTH0_DOMAIN || "withered-salad-9484.eu.auth0.com"}
-    clientId={env.REACT_APP_AUTH0_CLIENT || "VvT2n6cFCwL1pCTAxWdmZJuVPDwOGl3r"}
+    domain={
+      process.env.REACT_APP_AUTH0_DOMAIN || "withered-salad-9484.eu.auth0.com"
+    }
+    clientId={
+      process.env.REACT_APP_AUTH0_CLIENT || "VvT2n6cFCwL1pCTAxWdmZJuVPDwOGl3r"
+    }
     cacheLocation="localstorage"
     scope="openid email profile read:tipp write:tipp write:schedule"
     useRefreshTokens
     redirectUri={window.location.origin + window.location.pathname}
     audience={
-      env.REACT_APP_AUTH0_AUDIENCE || "https://nfl-tippspiel.herokuapp.com/auth"
+      process.env.REACT_APP_AUTH0_AUDIENCE ||
+      "https://nfl-tippspiel.herokuapp.com/auth"
     }
   >
     <React.StrictMode>
