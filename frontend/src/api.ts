e537b29b4dc28720f02b8189a421e837be4fb2bd
Florian Richter <florian.richter.de@gmail.com>
2021-07-09 09:24:59 +0200

    Rename env vars with prefix

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,4 @@
 import { env } from "process";
 
-export const BASE_URL = env.API_URL || "https://nfl-tippspiel.herokuapp.com/";
+export const BASE_URL =
+  env.REACT_APP_API_URL || "https://nfl-tippspiel.herokuapp.com/";
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,13 +7,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain={env.AUTH0_DOMAIN || "withered-salad-9484.eu.auth0.com"}
-    clientId={env.AUTH0_CLIENT || "VvT2n6cFCwL1pCTAxWdmZJuVPDwOGl3r"}
+    domain={env.REACT_APP_AUTH0_DOMAIN || "withered-salad-9484.eu.auth0.com"}
+    clientId={env.REACT_APP_AUTH0_CLIENT || "VvT2n6cFCwL1pCTAxWdmZJuVPDwOGl3r"}
     cacheLocation="localstorage"
     scope="openid email profile read:tipp write:tipp write:schedule"
     useRefreshTokens
     redirectUri={window.location.origin + window.location.pathname}
-    audience={env.AUTH0_AUDIENCE || "https://nfl-tippspiel.herokuapp.com/auth"}
+    audience={
+      env.REACT_APP_AUTH0_AUDIENCE || "https://nfl-tippspiel.herokuapp.com/auth"
+    }
   >
     <React.StrictMode>
       <App />
