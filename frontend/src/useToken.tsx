cbf7f93cbf8579712f9826fe54069e1fa41fe300
Florian Richter <florian.richter.de@gmail.com>
2021-09-17 09:43:52 +0200

    Automatically delete token if expired

--- frontend/src/useToken.tsx
+++ frontend/src/useToken.tsx
@@ -4,6 +4,13 @@ export function useToken() {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = window.localStorage.getItem("access_token");
+      if (item) {
+        const payload = JSON.parse(window.atob(item.split(".")[1]));
+        if (new Date(payload.exp * 1000) <= new Date()) {
+          window.localStorage.setItem("access_token", "");
+          return "";
+        }
+      }
       return item ? item : "";
     } catch (error) {
       console.log(error);
