1e75bfd0be6e8a559a585562ee97321adfdc6248
Florian Richter <flori@hey.com>
2023-01-12 14:05:10 +0100

    Fix division cell borders

--- frontend/src/Leaderboard/DivisionCell.tsx
+++ frontend/src/Leaderboard/DivisionCell.tsx
@@ -3,6 +3,9 @@ import { divisionLeaderboardState } from "../State/states";
 
 function DivisionCell({ div, user }: { user: string; div: string }) {
   const bet = useRecoilValue(divisionLeaderboardState([user, div]));
+  const correct = [bet?.first, bet?.second, bet?.third, bet?.fourth].sort(
+    (a, b) => (a && b ? a?.playoffSeed - b?.playoffSeed : 0)
+  );
   const prefix = process.env.REACT_APP_IMG_URL;
 
   return (
@@ -12,7 +15,9 @@ function DivisionCell({ div, user }: { user: string; div: string }) {
           <img
             src={prefix + bet.first.logo}
             className={`p-1 inline-block ${
-              bet.points ? "border-green-500 border rounded" : ""
+              bet.first.id === correct[0]?.id && bet.points
+                ? "border-green-500 border rounded"
+                : ""
             }`}
             width="32"
             height="32"
@@ -28,7 +33,9 @@ function DivisionCell({ div, user }: { user: string; div: string }) {
             <img
               src={prefix + bet.second.logo}
               className={`p-1 inline-block ${
-                bet.points ? "border-green-500 border rounded" : ""
+                bet.second.id === correct[1]?.id && bet.points
+                  ? "border-green-500 border rounded"
+                  : ""
               }`}
               width="32"
               height="32"
@@ -45,7 +52,9 @@ function DivisionCell({ div, user }: { user: string; div: string }) {
             <img
               src={prefix + bet.third.logo}
               className={`p-1 inline-block ${
-                bet.points ? "border rounded border-green-500" : ""
+                bet.third.id === correct[2]?.id && bet.points
+                  ? "border rounded border-green-500"
+                  : ""
               }`}
               width="32"
               height="32"
@@ -62,7 +71,9 @@ function DivisionCell({ div, user }: { user: string; div: string }) {
             <img
               src={prefix + bet.fourth.logo}
               className={`p-1 inline-block ${
-                bet.points ? "border-green-500 border rounded" : ""
+                bet.fourth.id === correct[3]?.id && bet.points
+                  ? "border-green-500 border rounded"
+                  : ""
               }`}
               width="32"
               height="32"
