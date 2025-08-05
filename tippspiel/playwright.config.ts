5d97a70fbab904566867038dee4ea134b32b836a
Florian Richter <flori@hey.com>
2025-08-05 12:51:26 +0200

    Remove react testing library

--- tippspiel/playwright.config.ts
+++ tippspiel/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests",
+  testMatch: "tests/**/*.e2e.ts",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 0,
