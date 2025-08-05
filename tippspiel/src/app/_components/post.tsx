c0ed124b65896a1774a50269468fe4049ed21eeb
Florian Richter <flori@hey.com>
2025-08-05 12:02:51 +0200

    Add playwright

--- tippspiel/src/app/_components/post.tsx
+++ tippspiel/src/app/_components/post.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { api } from "~/trpc/react";
 
 export function LatestPost() {
-  const [latestPost] = api.post.getLatest.useSuspenseQuery();
+  // const [latestPost] = api.post.getLatest.useSuspenseQuery();
 
   const utils = api.useUtils();
   const [name, setName] = useState("");
@@ -18,11 +18,7 @@ export function LatestPost() {
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
+      <p>You have no posts yet.</p>
       <form
         onSubmit={(e) => {
           e.preventDefault();
